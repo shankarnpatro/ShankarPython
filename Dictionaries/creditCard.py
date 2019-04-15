566b033833641818e445112c28d03ec49fe56737
Shankar <shankarnarayanpatro@gmail.com>
2019-04-15 14:57:35 +0530

    Commit d creditCard

---
+++ Dictionaries/creditCard.py
@@ -0,0 +1,13 @@
+class creditCard:
+
+    def __init__(self, avlamt, duebil):
+        self.avlmat = avlamt
+        self.duebil = duebil
+
+    def creditAmount(self):
+        return self.avlmat - duebil
+
+    Name = input("Enter the name : ")
+    Card_no = int(input("Enter the card number : "))
+avlamt = int(input("Enter the Available Balance : "))
+duebil = int(input("Enter the due bill : "))
