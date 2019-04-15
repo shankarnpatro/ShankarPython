2c9c0dda261e5ffa6a55678edfb5b875e99bf686
Shankar <shankarnarayanpatro@gmail.com>
2019-04-15 16:01:03 +0530

    changed the commit

--- List/StartWithS.py
+++ List/StartWithS.py
@@ -3,12 +3,16 @@ var = input("Enter the state name :")
 States = ["Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh",
           "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jammu and Kashmir", "Jharkhand", "Karnataka", "Kerala",
           "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab",
-          "Rajasthan",
-          "Sikkim", "Tamil Nadu", "Hyderabad", "Uttar Pradesh", "Uttarakhand", "West Bengal"]
-Start = []
+          "Rajasthan","Sikkim", "Tamil Nadu", "Hyderabad", "Uttar Pradesh", "Uttarakhand", "West Bengal"]
 
-for i in States:
-    if i[0] == var:
-        Start.append(i)
+class state:
+    def __init__(self):
+        Start = []
 
-print(Start)
+        for i in States:
+    
+            if i[0] == var:
+                Start.append(i)
+        print(Start)
+s1= state()
+print(s1)
